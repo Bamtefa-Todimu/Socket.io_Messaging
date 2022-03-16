be5737dbdff190a3c7dc56a002186f35d2cb1125
Bamtefa Todimu <84705311+Bamtefa-Todimu@users.noreply.github.com>
2022-03-16 19:57:30 +0100

    multer

--- server/routes/user.js
+++ server/routes/user.js
@@ -9,7 +9,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination:function (req, file, cb) {
-    cb(null, 'https://heuristic-keller-7238af.netlify.app/uploads')
+    cb(null, '../uploads')
   },
 
   filename:function(req,file,cb)
